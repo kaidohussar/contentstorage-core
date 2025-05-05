083b0574d929362f83fda9312a7f6994184329c1
Kaido Hussar <kaido@onoffapp.com>
2025-05-05 21:19:05 +0300

    up

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { loadConfig } from './lib/configLoader'
+export type { AppConfig } from './lib/configLoader'
+
+export { generateTypes } from './scripts/generate-types';
+export { pullContent } from './scripts/pull-content';
+
