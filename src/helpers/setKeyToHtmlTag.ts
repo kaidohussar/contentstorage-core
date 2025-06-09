8ce6f5438eeff82600468d1f2101c03a42b72b06
kaidohussar <kaidohus@gmail.com>
2025-06-09 09:21:06 +0300

    up

--- src/helpers/setKeyToHtmlTag.ts
+++ src/helpers/setKeyToHtmlTag.ts
@@ -3,6 +3,7 @@ const htmlElem = document.documentElement;
 export const setKeyToHtmlTag = (contentKey: string) => {
   const currentKeys = htmlElem.getAttribute('data-contentstorage-keys');
   let storedKeys = [];
+
   try {
     storedKeys = JSON.parse(currentKeys || '');
     if (!Array.isArray(storedKeys)) {
